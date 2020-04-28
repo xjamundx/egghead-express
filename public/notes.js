d53e35fad83f90ed606eae8767519b384416d71e
Jamund Ferguson <jamundf@amazon.com>
2020-04-28 10:35:03 -0700

    saving things

--- public/notes.js
+++ public/notes.js
@@ -1,12 +1,18 @@
 // dom elements
 const markdown = document.getElementById("markdown");
 const output = document.getElementById("output");
+const save = document.getElementById("save");
 
 // register events
 document.getElementById("markdown").addEventListener("input", handleChange);
+document.getElementById("save").addEventListener("click", handleSave);
 
 // helper functions
 function handleChange(e) {
   const html = marked(e.target.value);
   output.innerHTML = html;
+}
+
+function handleSave(e) {
+  e.target.disabled = true;
 }
